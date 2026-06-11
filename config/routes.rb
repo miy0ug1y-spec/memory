d2355b21f311a7b4c17792eca9ff6626eceb488c
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-06-11 04:25:16 +0000

    [Change]memory

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     resources :comments, only:[:index, :new,:create, :destroy]
  	end
   resources :users,path: 'users',path_names: { new: 'sign_up' }
-		
+	get 'mypage' => "users#mypage"	
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
