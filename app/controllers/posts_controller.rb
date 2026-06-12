a9afebad9f28db3d6dcc87946aec933f5ab6dd82
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-06-12 07:55:54 +0000

    [Change]memory

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,24 +5,41 @@ class PostsController < ApplicationController
   end
 
   def show
-  
+    @post = Post.find(params[:id])  
+  end
+
+  def mypost
+     @posts = Current.user.posts
   end
   
   def new
     @post = Post.new
   end
   
+  
   def create
     @post = Post.new(post_params)
     @post.user_id = Current.user.id
-    @post.save
-    redirect_to post_path(Current.user)
+
+    if @post.save
+      if @post.is_publish?
+        redirect_to root_path
+      else
+        redirect_to mypost_path
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   def edit
+    @post = Post.find(params[:id])
   end
   
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)  
   end
   
   def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :image
   scope :published, -> { where(is_publish: true) }
+  scope :unpublished, -> { where(is_publish: false) }
 
 end
