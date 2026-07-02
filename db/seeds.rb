495daca5d727357774f268206b8a32a61e006326
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-07-02 05:35:45 +0000

    [Change]seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "seedの実行を開始"
 
 users = [
   { 
@@ -131,3 +132,5 @@ posts.each do |data|
     )
   end
 end
+
+puts "seedの実行が完了しました"
