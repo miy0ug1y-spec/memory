d151ae61f4a9052b4616df110882c7c5e6339b00
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-07-20 10:59:35 +0000

    [Change]session controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,9 +6,12 @@ class Admin::UsersController < Admin::ApplicationController
 
   def withdraw
     @user = User.find(params[:id])
-    @user.update(is_active: false)
-
-    redirect_to admin_user_path(@user), notice:"ユーザーを退会状態にしました"
+    if @user.update(is_active: false)
+      @user.sessions.destroy_all
+      redirect_to admin_user_path(@user), notice:"ユーザーを退会状態にしました"
+    else
+      redirect_to admin_user_path(@user), alert: "退会処理に失敗しました"
+    end
   end
 
   def activate
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate_by(params.require(:session).permit(:email_address, :password))
-      start_new_session_for user
-      redirect_to after_authentication_url
-    else
-      redirect_to new_session_path, alert: "Try another email address or password."
+    user = User.authenticate_by(email_address: params[:session][:email_address],password: params[:session][:password])
+      if user && user.is_active
+       start_new_session_for user
+       redirect_to after_authentication_url
+      else
+      redirect_to new_session_path, alert: "メールアドレスまたはパスワードが正しくないか、退会済みのアカウントです"
     end
   end
 
