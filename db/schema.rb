a8733cb25c294bb14b40ef51dafa4f895cff5474
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-07-24 12:58:47 +0000

    [Add]group_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_23_021551) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_24_125012) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -83,6 +83,25 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_23_021551) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "group_memberships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_memberships_on_group_id"
+    t.index ["user_id", "group_id"], name: "index_group_memberships_on_user_id_and_group_id", unique: true
+    t.index ["user_id"], name: "index_group_memberships_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "introduction"
+    t.integer "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_groups_on_owner_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -139,6 +158,9 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_23_021551) do
   add_foreign_key "ending_posts", "posts"
   add_foreign_key "endings", "posts"
   add_foreign_key "endings", "users"
+  add_foreign_key "group_memberships", "groups"
+  add_foreign_key "group_memberships", "users"
+  add_foreign_key "groups", "users", column: "owner_id"
   add_foreign_key "posts", "genres"
   add_foreign_key "posts", "users"
   add_foreign_key "relationships", "users", column: "followed_id"
