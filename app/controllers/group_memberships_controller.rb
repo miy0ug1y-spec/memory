9e3c2b888d6e9af9518fb4cbf2ccb218c3a3420d
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-07-25 12:56:35 +0000

    [Change]Add group_rooting #15

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,2 +1,23 @@
 class GroupMembershipsController < ApplicationController
+
+  def create
+    group = Group.find(params[:group_id])
+    Current.user.group_memberships.create(
+      group: group
+    )
+
+    redirect_to group_path(group), notice: "グループに参加しました"
+  end
+
+  def destroy
+    group = Group.find(params[:group_id])
+
+    membership = Current.user.group_memberships.find_by(group: group)
+
+    membership&.destroy
+
+    redirect_to group_path(group), notice: "グループから退出しました"
+  end
+
+
 end
