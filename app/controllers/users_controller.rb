8a86f91c0001c172d72e4db1fd778964d56b1b66
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-07-31 08:22:08 +0000

    [Change]update mypage #16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_image = @user.image 
+    @posts = @user.posts.published.with_attached_image.order(created_at: :desc)
   end
 
   def update
